93982e632bd158b29e8700ad4f7865b5893e90b0
GabrielMM182 <gmm_morais@hotmail.com>
2024-12-25 12:49:21 -0300

    initial config with docker

---
+++ src/main/java/com/picpay/picpayTerceiraVersao/PicpayTerceiraVersaoApplication.java
@@ -0,0 +1,13 @@
+package com.picpay.picpayTerceiraVersao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpayTerceiraVersaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpayTerceiraVersaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/picpay/picpayTerceiraVersao/PicpayTerceiraVersaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picpay.picpayTerceiraVersao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpayTerceiraVersaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
